c4a5f588e9bd1250ee2f7ba431613c127a5e902a
IKolmykov <zabyz1337@gmail.com>
2026-02-20 10:21:53 +0100

    add booking layout

---
+++ src/components/EventBooking.js
@@ -0,0 +1,11 @@
+import EventDetails from "./EventDetails";
+import SeatSelector from "./SeatSelector";
+
+export default function EventBooking() {
+  return (
+    <div>
+      <EventDetails />
+      <SeatSelector />
+    </div>
+  );
+}
