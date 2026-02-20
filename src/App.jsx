3bbdf085ff27d2c4c3f0fe2a2612565c5ef59513
IKolmykov <zabyz1337@gmail.com>
2026-02-20 10:24:21 +0100

    wrap app into EventLookProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import EventBooking from "./components/EventBooking";
+import { EventBookingProvider } from "./context/EventBookingContext";
 
-function App() {
-  const [count, setCount] = useState(0)
+const eventsData = [
+  {
+    id: 237632,
+    date: new Date("2026-03-21"),
+    events: [
+      {
+        id: 7843687,
+        title: "Gorvin Show",
+        seats: [
+          { id: 1, label: "2a", isSelected: false },
+          { id: 2, label: "2b", isSelected: false },
+          { id: 3, label: "2c", isSelected: true },
+        ],
+      },
+    ],
+  },
+];
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <EventBookingProvider eventsData={eventsData}>
+      <EventBooking />
+    </EventBookingProvider>
+  );
 }
-
-export default App
